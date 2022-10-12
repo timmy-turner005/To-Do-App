d51f8b26c94cec558c6acd333fe1cd5096001ebd
timmy turner <timitalented005@gmail.com>
2022-10-12 15:26:56 +0100

    second commit

--- practice/src/components/ModuleComponent.js
+++
@@ -1,6 +0,0 @@
-import styles from "./style.module.css";
-const ModuleComponent = () => {
-    return <div className={styles.header}>turn this to red bitch</div>
-}
-
-export default ModuleComponent;
